6eb5a09e3965a5bb290fb9126b68d7fa2507d03d
Maya Blevins <maya.blevins@gmail.com>
2020-05-07 01:00:06 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,57 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+   cart.each do |product|
+    coupons.each do |coupon_product|
+      if product[:item] === coupon_product[:item]
+        new_product = {}
+        new_product[:item] = product[:item] + " W/COUPON"
+        new_product[:price] = coupon_product[:cost] / (coupon_product[:num])
+        new_product[:clearance] = product[:clearance]
+        
+        if product[:count] >= coupon_product[:num]
+          new_product[:count] = coupon_product[:num]
+          product[:count] -= coupon_product[:num]
+          cart << new_product
+        end
+      end
+    end
+  end
+  cart
 end
+
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  cart.each do |product|
+    if product[:clearance] === true
+      product[:price] = (product[:price] * 0.8).round(3)
+    end
+  end
+  cart
 end
 
+
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  total = 0
+  new_cart = consolidate_cart(cart)
+
+  
+  #cart size
+  if (new_cart.length === 1 && coupons.empty?) || (new_cart.length > 1 && coupons.empty?)
+    apply_clearance(new_cart)
+  end
+  
+  if (new_cart.length === 1 && coupons.length === 1) || (new_cart.length >= 1 && coupons.length >= 1)
+    apply_coupons(new_cart, coupons)
+    apply_clearance(new_cart)
+  end
+
+  new_cart.each do |product|
+    total += (product[:price] * product[:count])
+  end
+  if total > 100
+    total = total * 0.9
+  end
+  total
 end
+
